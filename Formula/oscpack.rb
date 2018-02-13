2210437855f729e591acdf7e303f8aa7f1f09ee8
Léo Colombaro <git@colombaro.fr>
2018-02-13 16:29:53 +0100

    switch to real formulae

--- Formula/oscpack.rb
+++ Formula/oscpack.rb
@@ -3,17 +3,17 @@ class Oscpack < Formula
   homepage "https://code.google.com/archive/p/oscpack/"
   url "https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/oscpack/oscpack_1_1_0.zip"
   sha256 "8389db649ed0a47b52bffe60aeec5157d192f59b4870d7487425d75032b05060"
-  revision 1
 
   needs :cxx11
 
   def install
     ENV.cxx11
 
-    mkdir "build" do
-      system "make"
-      system "make", "install-local"
-    end
+    system "make"
+    system "make", "install-local"
+
+    include.install Dir["oscpack/include/*"]
+    lib.install Dir["oscpack/lib/*"]
   end
 
   test do
