a7fc5e263a9239c2d4ad08052f20160e391b464b
Léo Colombaro <git@colombaro.fr>
2018-04-03 15:37:25 +0200

    bump opencv

--- Formula/opencv-gst.rb
+++ Formula/opencv-gst.rb
@@ -1,9 +1,9 @@
 class OpencvGst < Formula
   desc "Open source computer vision library"
   homepage "https://opencv.org/"
-  url "https://github.com/opencv/opencv/archive/3.4.0.tar.gz"
-  sha256 "678cc3d2d1b3464b512b084a8cca1fad7de207c7abdf2caa1fed636c13e916da"
-  revision 1
+  url "https://github.com/opencv/opencv/archive/3.4.1.tar.gz"
+  sha256 "f1b87684d75496a1054405ae3ee0b6573acaf3dad39eaf4f1d66fdd7e03dc852"
+  revision 2
 
   conflicts_with "opencv", :because => "same library"
 
@@ -12,33 +12,33 @@ class OpencvGst < Formula
   depends_on "eigen"
   depends_on "ffmpeg"
   depends_on "gstreamermm"
-  depends_on "ffmpeg"
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "libtiff"
   depends_on "openexr"
   depends_on "python"
-  depends_on "python3"
+  depends_on "python@2"
   depends_on "numpy"
   depends_on "tbb"
 
   needs :cxx11
 
   resource "contrib" do
-    url "https://github.com/opencv/opencv_contrib/archive/3.4.0.tar.gz"
-    sha256 "699ab3eee7922fbd3e8f98c68e6d16a1d453b20ef364e76172e56466dc9c16cd"
+    url "https://github.com/opencv/opencv_contrib/archive/3.4.1.tar.gz"
+    sha256 "298c69ee006d7675e1ff9d371ba8b0d9e7e88374bb7ba0f9d0789851d352ec6e"
   end
 
   def install
     ENV.cxx11
+    ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
 
     resource("contrib").stage buildpath/"opencv_contrib"
 
     # Reset PYTHONPATH, workaround for https://github.com/Homebrew/homebrew-science/pull/4885
     ENV.delete("PYTHONPATH")
 
-    py_prefix = `python-config --prefix`.chomp
-    py_lib = "#{py_prefix}/lib"
+    py2_prefix = `python2-config --prefix`.chomp
+    py2_lib = "#{py2_prefix}/lib"
 
     py3_config = `python3-config --configdir`.chomp
     py3_include = `python3 -c "import distutils.sysconfig as s; print(s.get_python_inc())"`.chomp
@@ -72,8 +72,8 @@ class OpencvGst < Formula
       -DBUILD_opencv_python2=ON
       -DBUILD_opencv_python3=ON
       -DPYTHON2_EXECUTABLE=#{which "python"}
-      -DPYTHON2_LIBRARY=#{py_lib}/libpython2.7.dylib
-      -DPYTHON2_INCLUDE_DIR=#{py_prefix}/include/python2.7
+      -DPYTHON2_LIBRARY=#{py2_lib}/libpython2.7.dylib
+      -DPYTHON2_INCLUDE_DIR=#{py2_prefix}/include/python2.7
       -DPYTHON3_EXECUTABLE=#{which "python3"}
       -DPYTHON3_LIBRARY=#{py3_config}/libpython#{py3_version}.dylib
       -DPYTHON3_INCLUDE_DIR=#{py3_include}
@@ -103,7 +103,7 @@ class OpencvGst < Formula
     system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-o", "test"
     assert_equal `./test`.strip, version.to_s
 
-    ["python", "python3"].each do |python|
+    ["python2.7", "python3"].each do |python|
       output = shell_output("#{python} -c 'import cv2; print(cv2.__version__)'")
       assert_equal version.to_s, output.chomp
     end
